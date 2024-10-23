b14c2804ee5029854300948d4e2f4bb4f18cd025
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-10-22 20:12:06 -0600

    ya sirve mis botones menu principal , tienda , gameplay , y salir del juego en el editor y cuando haya el build final del juego

--- Assets/Code/MenuManager.cs
+++ Assets/Code/MenuManager.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting.FullSerializer.Internal.Converters;
 
 public class MenuManager : MonoBehaviour {
+    #region Variables
 
+    public GameObject misionSelect, shopMenu, exitGO;
+    public Button MainMenu, missionSelect, exit;
 
-    private GameObject menuPanel;
+    #endregion Variables
 
-    public MenuManager(GameObject panel) {
-        menuPanel = panel;
+    #region Funciones Publicas 
+    // Métodos para cambiar entre escenas
+    public void LoadMenuPrincipal() {
+        SceneManager.LoadScene("Menu");
     }
 
-    public void ToggleMenu() {
-        menuPanel.SetActive(!menuPanel.activeSelf);
+    public void LoadSeleccionMisiones() {
+        SceneManager.LoadScene("MisionSelect");
     }
+
+    public void LoadTienda() {
+        SceneManager.LoadScene("Tienda");
+    }
+
+    public void LoadGameplay() {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void QuitGame() {
+
+        // Si estás en el editor de Unity, detiene el modo de juego
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+
+    #endregion Funciones Publicas
+
 }
+
 
 
