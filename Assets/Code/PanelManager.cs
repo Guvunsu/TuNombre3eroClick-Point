41a9a5731fee52f546fd8bf380ceea0db7fa42b2
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-11-18 23:56:27 -0600

    fgjh

--- Assets/Code/PanelManager.cs
+++ Assets/Code/PanelManager.cs
@@ -4,109 +4,12 @@ using TMPro; // Para el TextMeshProUGUI
 using Cinemachine;
 using static UnityEngine.Rendering.DebugUI;
 
-public class PanelManager : MonoBehaviour
-{
+public class PanelManager : MonoBehaviour {
     #region Variables generales
 
     private bool paused = false;  // Para saber si el juego está en pausa
 
     #endregion Variables generales
-
-    #region Funciones Start y Update
-
-    void Start()
-    {
-        // Inicializamos la escena
-        ActivatePanel(setIndex);
-
-        // Inicializamos los paneles (asegurándonos que el panel de victoria está desactivado)
-        panelVictoria.SetActive(false);
-        pausePanel.SetActive(false);  // Inicialmente el panel de pausa está oculto
-
-        // Inicializa el tiempo restante con el tiempo límite
-        tiempoRestante = tiempoLimite;
-
-        //Ganar EStrellas
-
-        // Inicializamos el tiempo restante con el tiempo que empieza el nivel
-        tiempoRestante = tiempoMinimo;
-        estrellas = 0;  // Inicializamos las estrellas en 0
-        resultado = "Tiempo mínimo no alcanzado";
-        ResetearContadorBichos();
-    }
-
-    void Update()
-    {
-        // Activar o desactivar el menú de pausa cuando el jugador presiona "P"
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            if (paused)
-            {
-                ResumeGame();
-                ReanudarTimer();
-            }
-            else
-            {
-                PauseGame();
-                PausarTimer();
-            }
-
-        }
-
-        //// Verificamos si el nivel ha sido completado para mostrar el panel de victoria
-        if (NivelCompletado())
-        {
-            MostrarPanelVictoria();
-        }
-
-        // Si el timer está activo y no está pausado
-        if (timerActivo && !isPaused)
-        {
-            // Decrementamos el tiempo usando unscaledDeltaTime para que no se vea afectado por la pausa
-            tiempoRestante -= Time.unscaledDeltaTime;
-
-            // Si el tiempo llega a 0, detenemos el timer
-            if (tiempoRestante <= 0f)
-            {
-                tiempoRestante = 0f;
-                timerActivo = false;
-                // Aquí podrías activar alguna lógica cuando el timer llegue a 0, como finalizar el nivel
-            }
-
-            // Actualizamos el texto del timer en el UI
-            ActualizarTextoTimer();
-        }
-
-
-        // GanarEstrellas
-
-        // Si el tiempo restante es mayor a 0, reducimos el tiempo
-        if (tiempoRestante > 0)
-        {
-            tiempoRestante -= Time.deltaTime;
-        }
-        else
-        {
-            // Si pasamos el tiempo mínimo, asignamos una estrella
-            if (tiempoRestante <= 0 && estrellas == 0)
-            {
-                // Aquí verificamos si se pasó el tiempo mínimo y se puede ganar la estrella
-                if (tiempoRestante > -1)  // Si terminó dentro del tiempo
-                {
-                    estrellas = 1;
-                    resultado = "¡Ganaste 1 estrella!";
-                }
-                else
-                {
-                    resultado = "Tiempo mínimo no alcanzado";
-                }
-            }
-        }
-
-        // Actualizamos el texto de las estrellas
-        textoEstrellas.text = "Estrellas: " + estrellas;
-    }
-    #endregion Funciones Start y Update
 
     #region Variables para los paneles del gameplay
 
@@ -118,29 +21,6 @@ public class PanelManager : MonoBehaviour
 
     #endregion Variables para los paneles del gameplay
 
-    #region Funciones para los paneles del gameplay
-
-    public void MovePanel(int direction)
-    {
-        int newIndex = (setIndex + direction + panels.Length) % panels.Length;
-        ActivatePanel(newIndex);
-    }
-
-    private void ActivatePanel(int index)
-    {
-        // Desactiva todos los paneles excepto el actual
-        for (int i = 0; i < panels.Length; i++)
-        {
-            panels[i].SetActive(i == index);
-        }
-
-        // La cámara sigue al nuevo panel
-        vCam.Follow = panelTargets[setIndex];
-        setIndex = index;
-    }
-
-    #endregion Funciones para los paneles del gameplay
-
     #region Variables para el pausa panel
 
     public string menuSceneName = "Menu";
@@ -148,75 +28,6 @@ public class PanelManager : MonoBehaviour
     public string missionSelectSceneName = "MisionSelect";
 
     #endregion Variables para el pausa panel
-
-    #region Funciones para el panel de pausa
-
-    // Activa el menú de pausa
-    public void PauseGame()
-    {
-        paused = true;
-        pausePanel.SetActive(true);  // Activar el panel de pausa
-        Time.timeScale = 0f;         // Detiene el tiempo del juego
-
-        // Desactiva los paneles de gameplay, pero NO los borra
-        foreach (GameObject panel in panels)
-        {
-            panel.SetActive(false);
-        }
-    }
-
-    // Desactiva el menú de pausa
-    public void ResumeGame()
-    {
-        paused = false;
-        pausePanel.SetActive(false);  // Desactivar el panel de pausa
-        Time.timeScale = 1f;          // Reanuda el tiempo del juego
-
-        // Reactiva los paneles de gameplay
-        foreach (GameObject panel in panels)
-        {
-            panel.SetActive(true);
-        }
-    }
-
-    // Reinicia el nivel actual
-    public void RetryGame()
-    {
-        Time.timeScale = 1f;  // Asegura que el tiempo del juego esté en modo normal
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    // Carga la escena del menú principal
-    public void GoToMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(menuSceneName);
-    }
-
-    // Carga la escena de la tienda
-    public void GoToStore()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(tiendaSceneName);
-    }
-
-    // Carga la escena del selector de misiones
-    public void GoToMissionSelector()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(missionSelectSceneName);
-    }
-
-    // Salir del juego
-    public void GoToExit()
-    {
-        // Si estás en el editor de Unity, detiene el modo de juego
-        UnityEditor.EditorApplication.isPlaying = false;
-
-        Application.Quit();
-    }
-
-    #endregion Funciones para el panel de pausa
 
     #region Variables para el Panel de Victoria y obtener estrellas
 
@@ -238,84 +49,6 @@ public class PanelManager : MonoBehaviour
 
     #endregion Variables para el Panel de Victoria y obtener estrellas
 
-    #region Funciones Panel Victoria y obtener estrellas
-    //Método que se llama cuando el nivel ha sido completado
-    public bool NivelCompletado()
-    {
-        // si umplimos ambas condiciones de tiempo y destrucción de bichos se activa panel de victoria
-        MostrarPanelVictoria();
-        return estrellas > 0 || spawnRandomScript.bichosDestruidos == 8;
-    }
-
-    // Mostrar el panel de victoria y las estrellas obtenidas
-    public void MostrarPanelVictoria()
-    {
-        // Activamos el panel de victoria
-        panelVictoria.SetActive(true);
-
-        // Reseteamos las estrellas
-        totalEstrellas = 0;
-
-        // Verificamos el tiempo y ganas estrellas 
-        if (estrellas > 0)
-        {
-            totalEstrellas++;
-        }
-
-        // Verificamos las estrellas por la destrucción de bichos (de SpawnRandom)
-        if (spawnRandomScript.bichosDestruidos == 8)
-        {
-            totalEstrellas++;
-        }
-
-        // Actualizamos el texto de estrellas
-        textoEstrellas.text = "¡Has ganado " + totalEstrellas + " estrella(s)!";
-    }
-
-    // Método para cerrar el panel de victoria y reiniciar el estado del juego
-    public void CerrarPanelVictoria()
-    {
-        // Ocultamos el panel de victoria
-        panelVictoria.SetActive(false);
-
-        // Reseteamos los valores de las estrellas y bichos destruidos
-        ResetearEstrellas();
-        spawnRandomScript.bichosDestruidos = 0;  // Reseteamos los bichos destruidos
-    }
-    public void ResetearEstrellas()
-    {
-        // Reseteamos el valor de las estrellas y el tiempo cuando el jugador reinicia o vuelve a intentar el nivel
-        estrellas = 0;
-        tiempoRestante = tiempoMinimo;
-        resultado = "Tiempo mínimo no alcanzado";
-        textoEstrellas.text = "Estrellas: " + estrellas;
-    }
-    public void BichoDestruido(string bichoID)
-    {
-        // Incrementamos el contador de bichos destruidos cuando el bicho se destruye
-        // Cada enemigo debe tener un identificador único o tag, en este caso se asume que los enemigos 
-        // tienen un string o ID único que pasa a través del parámetro
-        if (bichosDestruidos < totalBichos)
-        {
-            bichosDestruidos++;
-            Debug.Log($"Bicho destruido: {bichoID}. Total destruidos: {bichosDestruidos}");
-
-            // Si el jugador ha destruido todos los bichos ganas 1 estrella
-            if (bichosDestruidos == totalBichos)
-            {
-                estrellas++;
-                Debug.Log("¡Ganaste 1 estrella por destruir a todos los bichos!");
-            }
-        }
-    }
-    public void ResetearContadorBichos()
-    {
-        // Reseteamos el contador a 0 cuando se reinicia el juego o se reinicia el nivel
-        bichosDestruidos = 0;
-    }
-
-    #endregion Funciones Panel Victoria y obtener estrellas
-
     #region Variables Texto de tiempo 
 
     [Header("Timer Settings")]
@@ -330,35 +63,266 @@ public class PanelManager : MonoBehaviour
 
     #endregion Variables Texto de tiempo
 
+    #region Funciones Start y Update
+
+    void Start() {
+        // Inicializamos la escena
+        ActivatePanel(setIndex);
+
+        // Inicializamos los paneles (asegurándonos que el panel de victoria está desactivado)
+        panelVictoria.SetActive(false);
+        pausePanel.SetActive(false);  // Inicialmente el panel de pausa está oculto
+
+        // Inicializa el tiempo restante con el tiempo límite
+        tiempoRestante = tiempoLimite;
+
+        //Ganar EStrellas
+
+        // Inicializamos el tiempo restante con el tiempo que empieza el nivel
+        tiempoRestante = tiempoMinimo;
+        estrellas = 0;  // Inicializamos las estrellas en 0
+        resultado = "Tiempo mínimo no alcanzado";
+        ResetearContadorBichos();
+    }
+
+    void Update() {
+        // Activar o desactivar el menú de pausa cuando el jugador presiona "P"
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (paused) {
+                ResumeGame();
+                ReanudarTimer();
+            } else {
+                PauseGame();
+                PausarTimer();
+            }
+
+        }
+
+        ////// Verificamos si el nivel ha sido completado para mostrar el panel de victoria
+        //if (NivelCompletado()) {
+        //    MostrarPanelVictoria();
+        //}
+
+        // Si el timer está activo y no está pausado
+        if (timerActivo && !isPaused) {
+            // Decrementamos el tiempo usando unscaledDeltaTime para que no se vea afectado por la pausa
+            tiempoRestante -= Time.unscaledDeltaTime;
+
+            // Si el tiempo llega a 0, detenemos el timer
+            if (tiempoRestante <= 0f) {
+                tiempoRestante = 0f;
+                timerActivo = false;
+                //deberia de poner aqui el panel de loose 
+            }
+
+            // Actualizamos el texto del timer en el UI
+            ActualizarTextoTimer();
+        }
+
+
+        // GanarEstrellas
+
+        // Si el tiempo restante es mayor a 0, reducimos el tiempo
+        if (tiempoRestante > 0) {
+            tiempoRestante -= Time.deltaTime;
+        } else {
+            // Si pasamos el tiempo mínimo, asignamos una estrella
+            if (tiempoRestante <= 0 && estrellas == 0) {
+                // Aquí verificamos si se pasó el tiempo mínimo y se puede ganar la estrella
+                if (tiempoRestante > -1)  // Si terminó dentro del tiempo
+                {
+                    estrellas = 1;
+                    resultado = "¡Ganaste 1 estrella!";
+                } else {
+                    resultado = "Tiempo mínimo no alcanzado";
+                }
+            }
+        }
+
+        // Actualizamos el texto de las estrellas
+        textoEstrellas.text = "Estrellas: " + estrellas;
+    }
+    #endregion Funciones Start y Update
+
+    #region Funciones para los paneles del gameplay
+
+    public void MovePanel(int direction) {
+        int newIndex = (setIndex + direction + panels.Length) % panels.Length;
+        ActivatePanel(newIndex);
+    }
+
+    private void ActivatePanel(int index) {
+        foreach (GameObject panel in panels) {
+            panel.SetActive(false);
+        }
+        if (index >= 0 && index < panels.Length) {
+            panels[index].SetActive(true);
+            setIndex = index;
+        }
+        //// Desactiva todos los paneles excepto el actual
+        //for (int i = 0; i < panels.Length; i++)
+        //{
+        //    panels[i].SetActive(i == index);
+        //}
+
+        // La cámara sigue al nuevo panel
+        vCam.Follow = panelTargets[setIndex];
+        setIndex = index;
+    }
+
+    #endregion Funciones para los paneles del gameplay
+
+    #region Funciones para el panel de pausa
+
+    // Activa el menú de pausa
+    public void PauseGame() {
+        paused = true;
+        pausePanel.SetActive(true);  // Activar el panel de pausa
+        Time.timeScale = 0f;         // Detiene el tiempo del juego
+
+        // Desactiva los paneles de gameplay, pero NO los borra
+        foreach (GameObject panel in panels) {
+            panel.SetActive(false);
+        }
+    }
+
+    // Desactiva el menú de pausa
+    public void ResumeGame() {
+        paused = false;
+        pausePanel.SetActive(false);  // Desactivar el panel de pausa
+        Time.timeScale = 1f;          // Reanuda el tiempo del juego
+
+        // Reactiva los paneles de gameplay
+        foreach (GameObject panel in panels) {
+            panel.SetActive(true);
+        }
+    }
+
+    // Reinicia el nivel actual
+    public void RetryGame() {
+        Time.timeScale = 1f;  // Asegura que el tiempo del juego esté en modo normal
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Carga la escena del menú principal
+    public void GoToMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    // Carga la escena de la tienda
+    public void GoToStore() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(tiendaSceneName);
+    }
+
+    // Carga la escena del selector de misiones
+    public void GoToMissionSelector() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(missionSelectSceneName);
+    }
+
+    // Salir del juego
+    public void GoToExit() {
+        // Si estás en el editor de Unity, detiene el modo de juego
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+
+    #endregion Funciones para el panel de pausa
+
+    #region Funciones Panel Victoria y obtener estrellas
+    //Método que se llama cuando el nivel ha sido completado
+    public bool NivelCompletado() {
+        // si umplimos ambas condiciones de tiempo y destrucción de bichos se activa panel de victoria
+        MostrarPanelVictoria();
+        return estrellas > 0 || spawnRandomScript.bichosDestruidos == 8;
+    }
+
+    // Mostrar el panel de victoria y las estrellas obtenidas
+    public void MostrarPanelVictoria() {
+        // Activamos el panel de victoria
+        panelVictoria.SetActive(true);
+
+        // Reseteamos las estrellas
+        totalEstrellas = 0;
+
+        // Verificamos el tiempo y ganas estrellas 
+        if (estrellas > 0) {
+            totalEstrellas++;
+        }
+
+        // Verificamos las estrellas por la destrucción de bichos (de SpawnRandom)
+        if (spawnRandomScript.bichosDestruidos == 8) {
+            totalEstrellas++;
+        }
+
+        // Actualizamos el texto de estrellas
+        textoEstrellas.text = "¡Has ganado " + totalEstrellas + " estrella(s)!";
+    }
+
+    // Método para cerrar el panel de victoria y reiniciar el estado del juego
+    public void CerrarPanelVictoria() {
+        // Ocultamos el panel de victoria
+        panelVictoria.SetActive(false);
+
+        // Reseteamos los valores de las estrellas y bichos destruidos
+        ResetearEstrellas();
+        spawnRandomScript.bichosDestruidos = 0;  // Reseteamos los bichos destruidos
+    }
+    public void ResetearEstrellas() {
+        // Reseteamos el valor de las estrellas y el tiempo cuando el jugador reinicia o vuelve a intentar el nivel
+        estrellas = 0;
+        tiempoRestante = tiempoMinimo;
+        resultado = "Tiempo mínimo no alcanzado";
+        textoEstrellas.text = "Estrellas: " + estrellas;
+    }
+    public void BichoDestruido(string bichoID) {
+        // Incrementamos el contador de bichos destruidos cuando el bicho se destruye
+        // Cada enemigo debe tener un identificador único o tag, en este caso se asume que los enemigos 
+        // tienen un string o ID único que pasa a través del parámetro
+        if (bichosDestruidos < totalBichos) {
+            bichosDestruidos++;
+            Debug.Log($"Bicho destruido: {bichoID}. Total destruidos: {bichosDestruidos}");
+
+            // Si el jugador ha destruido todos los bichos ganas 1 estrella
+            if (bichosDestruidos == totalBichos) {
+                estrellas++;
+                Debug.Log("¡Ganaste 1 estrella por destruir a todos los bichos!");
+            }
+        }
+    }
+    public void ResetearContadorBichos() {
+        // Reseteamos el contador a 0 cuando se reinicia el juego o se reinicia el nivel
+        bichosDestruidos = 0;
+    }
+
+    #endregion Funciones Panel Victoria y obtener estrellas
+
     #region Funciones texto de tiempo 
 
-    void ActualizarTextoTimer()
-    {
+    void ActualizarTextoTimer() {
         float minutos = Mathf.FloorToInt(tiempoRestante / 60);
         float segundos = Mathf.FloorToInt(tiempoRestante % 60);
         textoTimer.text = string.Format("{0:00}:{1:00}", minutos, segundos);
     }
-    public void DetenerTimer()
-    {
+    public void DetenerTimer() {
         timerActivo = false;
     }
-    public void ReiniciarTimer()
-    {
+    public void ReiniciarTimer() {
         tiempoRestante = tiempoLimite;
         timerActivo = true;
     }
-    public void CambiarTiempoLimite(float nuevoTiempo)
-    {
+    public void CambiarTiempoLimite(float nuevoTiempo) {
         tiempoLimite = nuevoTiempo;
         ReiniciarTimer();  // Reinicia el timer con el nuevo tiempo límite
     }
-    public void PausarTimer()
-    {
+    public void PausarTimer() {
         isPaused = true;
         Time.timeScale = 0f;  // Detiene el tiempo del juego (efecto global de pausa)
     }
-    public void ReanudarTimer()
-    {
+    public void ReanudarTimer() {
         isPaused = false;
         Time.timeScale = 1f;  // Restaura el tiempo del juego (efecto global de reanudación)
     }
